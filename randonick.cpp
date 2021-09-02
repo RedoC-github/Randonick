a0e7a3f5bf86b550a9f33b36f4c4df8204af5c40
Junhyuk Han <jhhan128@icloud.com>
2021-09-02 16:16:22 +0900

    Changed Random engine

--- randonick.cpp
+++ randonick.cpp
@@ -1,95 +1,107 @@
+// 2021 yuanie324. All rights reserved.
+
 #include <iostream>
 #include <random>
 #include <vector>
 #include <algorithm>
 #include <string>
 
+unsigned int getRandomNumber(void);
+std::string generateNick(const int, const int, const int);
+
 int main(int argc, const char **argv) {
-
-    // 난수 생성 with mt19937
-
-    std::mt19937 mt;
-    mt.seed((unsigned int)time(0));
     
-    // 입력
-
-    start:
-
-    std::cout << "옵션을 선택하세요(1~3 숫자 입력)\n\n";
-    std::cout << "1 알파벳 + 숫자\n";
-    std::cout << "2 숫자 + 알파벳\n";
-    std::cout << "3 알파벳 only\n\n";
-    std::cout << ">> ";
-
-    int option;
-    std::cin >> option;
-
+    // input
+    bool flag = false;
     int alphabetLength = 0, numberLength = 0;
+    int option;
 
-    switch (option) {
-        case 1:
-            std::cout << "알파벳 길이를 입력하세요 >> ";
-            std::cin >> alphabetLength;
+    while (!flag) {
+        std::cout << "Please choose an option(1 to 3)\n\n";
+        std::cout << "1 Alphabets + Numbers\n";
+        std::cout << "2 Numbers + Alphabets\n";
+        std::cout << "3 Alphabets only\n\n";
+        std::cout << ">> ";
 
-            std::cout << "숫자 길이를 입력하세요 >> ";
-            std::cin >> numberLength;
-            break;
+        std::cin >> option;
 
-        case 2:
-            std::cout << "숫자 길이를 입력하세요 >> ";
-            std::cin >> numberLength;
+        switch (option) {
+            case 1:
+                std::cout << "Enter the number of alphabets >> ";
+                std::cin >> alphabetLength;
 
-            std::cout << "알파벳 길이를 입력하세요 >> ";
-            std::cin >> alphabetLength;
-            break;
+                std::cout << "Enter the number of numbers >> ";
+                std::cin >> numberLength;
 
-        case 3:
-            std::cout << "알파벳 길이를 입력하세요 >> ";
-            std::cin >> alphabetLength;
-            break;
+                flag = true;
+                break;
 
-        default:
-            std::cout << "\n잘못된 옵션입니다.\n\n";
-            goto start;
+            case 2:
+                std::cout << "Enter the number of numbers >> ";
+                std::cin >> numberLength;
+
+                std::cout << "Enter the number of alphabets >> ";
+                std::cin >> alphabetLength;
+
+                flag = true;
+                break;
+
+            case 3:
+                std::cout << "Enter the number of alphabets >> ";
+                std::cin >> alphabetLength;
+
+                flag = true;
+                break;
+
+            default:
+                std::cout << "\nError: Undefined option.\n\n";
+        }
     }
 
-    // 닉네임 생성
+    // generate nickname
+    std::string nickname = generateNick(option, alphabetLength, numberLength);
+    std::cout << "\nYour nickname is:\n" << nickname << '\n';
+
+    return 0;
+}
+
+unsigned int getRandomNumber(void) {
+    std::random_device random;
+    std::mt19937 engine(random());
+    std::uniform_int_distribution<unsigned int> __rand(0, 100000000);
+
+    return (unsigned int)__rand(engine);
+}
+
+std::string generateNick(const int opt, const int al, const int nu) {
+    std::string ret = "";
 
     std::vector<char> number;
     std::vector<char> alphabet;
 
-    for (char c = 'a'; c <= 'z'; c++) {
-        alphabet.push_back(c);
-    }
+    for (char c = '0'; c <= '9'; c++) number.push_back(c);
+    for (char c = 'a'; c <= 'z'; c++) alphabet.push_back(c);
 
-    for (char c = '0'; c <= '9'; c++) {
-        number.push_back(c);
-    }
-
-    std::string num = "";
     std::string alpha = "";
+    std::string num = "";
 
-    for (int i = 0; i < numberLength; i++) {
-        int rand_ = mt() % (unsigned int)number.size();
-        num.push_back(number[rand_]);
-    }
-
-    for (int i = 0; i < alphabetLength; i++) {
-        int rand_ = mt() % (unsigned int)alphabet.size();
+    for (int i = 0; i < al; i++) {
+        int rand_ = getRandomNumber() % (unsigned int)(alphabet.size());
         alpha.push_back(alphabet[rand_]);
     }
 
-    std::string result;
-
-    if (option == 1) {
-        result = alpha + num;
-    } else if (option == 2) {
-        result = num + alpha;
-    } else {
-        result = alpha;
+    for (int i = 0; i < nu; i++) {
+        int rand_ = getRandomNumber() % (unsigned int)(number.size());
+        num.push_back(number[rand_]);
     }
 
-    std::cout << '\n' << result << '\n';
+    if (opt == 1) {
+        ret = alpha + num;
+    } else if (opt == 2) {
+        ret = num + alpha;
+    } else if (opt == 3) {
+        ret = alpha;
+    }
 
-    return 0;
+    return ret;
 }
